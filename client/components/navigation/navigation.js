3ff52fd05797817506b13eb01da0dc9568a696e3
TRomesh <tharakaromesh@gmail.com>
2017-06-02 14:49:23 +0530

    add Link to home an account

--- client/components/navigation/navigation.js
+++ client/components/navigation/navigation.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import AppBar from 'material-ui/AppBar';
 import FlatButton from 'material-ui/FlatButton';
 import IconButton from 'material-ui/IconButton';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 const styles = {
   title: {
@@ -19,7 +20,12 @@ class Navigation extends Component{
        <AppBar
          title={<span style={styles.title}>RecipesHub</span>}
          iconElementLeft={<IconButton></IconButton>}
-         iconElementRight={<FlatButton label="Save" />}/>
+         iconElementRight={
+           <div style={{marginRight:'0px',marginTop:'8px'}}>
+             <Link to="/home"><FlatButton label="Home" /></Link>
+             <Link to="/account"><FlatButton label="Account" /></Link>
+           </div>
+       }/>
          {this.props.children}
        </div>
      );
