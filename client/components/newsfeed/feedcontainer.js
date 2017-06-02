af6ea30363abe599e397f493284181670608519d
MadushikaPerera <madushikad.perera@gmail.com>
2017-06-02 09:40:15 +0530

    Add feed container and modify newsfeed

--- client/components/newsfeed/feedcontainer.js
+++ client/components/newsfeed/feedcontainer.js
@@ -4,14 +4,47 @@ import {connect} from 'react-redux';
 import { withRouter } from 'react-router-dom'
 import {bindActionCreators} from 'redux';
 import * as RecipeActions from '../../actions/RecipeActions';
-
+import Newsfeeds from './newsfeed';
 
 class FeedContainer extends Component{
+
+  constructor(props) {
+    super(props);
+      this.state = {
+
+      };
+  }
+
+  _renderItem = () => {
+      // return this.state.results.map((result) => {
+      //   return (<Newsfeeds key={result.id}
+      //                      id={result.id}
+      //                      name={result.name}
+      //                      type={result.type}
+      //                      description={result.description}
+      //                      user_id={result.user_id}
+      //                      user_name={result.user_name}
+      //                      attachment={result.attachment}
+      //                      created_at={result.created_at}
+      //                      lkedpost={result.liked}/>);
+      // });
+
+      return (<Newsfeeds  key=1
+                          id=1
+                          name='Pizza'
+                          type='Pizza'
+                          description='aaaaaaaaaaaaaaaaaaaaaaa'
+                          user_id=4
+                          user_name='Madushika'
+                          attachment='http://ptownpizza.com/images/Food/Pizza/Pizza01.jpg'
+                          created_at='2017.06.02'
+                          lkedpost='true'/>);
+    },
 
   render(){
     return(
       <div>
-
+          {this._renderItem()}
       </div>
     );
   }
--- client/components/newsfeed/newsfeed.js
+++ client/components/newsfeed/newsfeed.js
@@ -5,96 +5,186 @@ import { withRouter } from 'react-router-dom'
 import {bindActionCreators} from 'redux';
 import * as RecipeActions from '../../actions/RecipeActions';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import StarBorder from 'material-ui/svg-icons/toggle/star-border';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import FavIcon from 'material-ui/svg-icons/action/favorite';
+import FavIconBorder from 'material-ui/svg-icons/action/favorite-border';
+import {deepOrange400} from 'material-ui/styles/colors';
+import {List, ListItem} from 'material-ui/List';
+import Avatar from 'material-ui/Avatar';
 import FlatButton from 'material-ui/FlatButton';
+import Dialog from 'material-ui/Dialog';
+import TextField from 'material-ui/TextField';
 
 const Cardstyle = {
-  height: 500,
+  height: 450,
   width: 300,
   marginTop: 30,
-  marginLeft: 20,
+  //marginLeft: 20,
 };
+
 
 class Newsfeeds extends Component{
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      editOpen: false,
+      deleteOpen: false,
+      recipieOpen: false,
+    };
+  }
+
+  _editStatus = () => {
+    // let status = this.refs.EditBox.getValue();
+    // let editData = {
+    //   userId: localStorage.getItem('userid'),
+    //   postId: this.props.id,
+    //   status: status,
+    // };
+    // ActivityfeedAction._editStatus(editData);
+    this.handleClose();
+  }
+
+  _deleteStatus = () => {
+    // let deleteData = {
+    //   userId: localStorage.getItem('userid'),
+    //   postId: this.props.id,
+    // };
+    // ActivityfeedAction._deleteStatus(deleteData);
+    }
+
+  handleOpen = () => {
+    this.setState({editOpen: true});
+  }
+
+  handleOpenDelete = () => {
+    this.setState({deleteOpen: true});
+  }
+
+  handleOpenRecipie = () => {
+    this.setState({recipieOpen: true});
+  }
+
+  handleClose = () => {
+     this.setState({editOpen: false});
+     this.setState({deleteOpen: false});
+     this.setState({recipieOpen: false});
+   }
+
   render(){
+
+    const updateActions = [
+      <FlatButton
+        label="Update"
+        primary={true}
+        keyboardFocused={true}
+        onTouchTap={this._editStatus}/>,
+
+      <FlatButton
+        label="Close"
+        secondary={true}
+        onTouchTap={this.handleClose}/>,
+    ];
+
+    const confirmDeleteActions = [
+      <FlatButton
+        label="Delete"
+        primary={true}
+        keyboardFocused={true}
+        onTouchTap={this._deleteStatus}/>,
+
+      <FlatButton
+        label="Cansel"
+        secondary={true}
+        onTouchTap={this.handleClose}/>,
+    ];
+
+    const showRecipieActions = [
+      <FlatButton
+        label="Ok"
+        secondary={true}
+        onTouchTap={this.handleClose}/>,
+    ];
+
     return(
       <div className="column">
-        <Card style={Cardstyle} className="col-lg-3">
-          <CardHeader
-            title="URL Avatar"
-            subtitle="Subtitle"
-            avatar="images/jsa-128.jpg"
-          />
-          <CardMedia>
-            <img src="https://i.ytimg.com/vi/zdpJy70Ou48/maxresdefault.jpg" />
-          </CardMedia>
-          <CardTitle titleStyle={{fontSize: 20}} title="Card title" subtitle="Card subtitle" />
-          <CardText>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-          </CardText>
-          <CardActions>
-          </CardActions>
-        </Card>
-        <Card style={Cardstyle} className="col-lg-3">
-          <CardHeader
-            title="URL Avatar"
-            subtitle="Subtitle"
-            avatar="images/jsa-128.jpg"
-          />
-          <CardMedia>
-            <img src="https://i.ytimg.com/vi/zdpJy70Ou48/maxresdefault.jpg" />
-          </CardMedia>
-          <CardTitle titleStyle={{fontSize: 20}} title="Card title" subtitle="Card subtitle" />
-          <CardText>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-          </CardText>
-          <CardActions>
-          </CardActions>
-        </Card>
-        <Card style={Cardstyle} className="col-lg-3">
-          <CardHeader
-            title="URL Avatar"
-            subtitle="Subtitle"
-            avatar="images/jsa-128.jpg"
-          />
-          <CardMedia>
-            <img src="https://i.ytimg.com/vi/zdpJy70Ou48/maxresdefault.jpg" />
-          </CardMedia>
-          <CardTitle titleStyle={{fontSize: 20}} title="Card title" subtitle="Card subtitle" />
-          <CardText>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-          </CardText>
-          <CardActions>
-          </CardActions>
-        </Card>
-        <Card style={Cardstyle} className="col-lg-3">
-          <CardHeader
-            title="URL Avatar"
-            subtitle="Subtitle"
-            avatar="images/jsa-128.jpg"
-          />
-          <CardMedia>
-            <img src="https://i.ytimg.com/vi/zdpJy70Ou48/maxresdefault.jpg" />
-          </CardMedia>
-          <CardTitle titleStyle={{fontSize: 20}} title="Card title" subtitle="Card subtitle" />
-          <CardText>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-          </CardText>
-          <CardActions>
-          </CardActions>
-        </Card>
+        <div className="col-lg-3">
+          <Card style={Cardstyle}>
+            <ListItem
+              leftAvatar={<Avatar src='http://aurora-awards.com/wp-content/uploads/2017/05/girls-hd-images-cute-girl-hd-wallpaper-cnmuqi.jpg' />}
+              primaryText='Madushika Perera'
+              secondaryText={
+                <p>
+                  created_at
+                </p>
+              }
+              secondaryTextLines={1}
+              rightIconButton={
+                  <IconMenu iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+                            anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+                            targetOrigin={{horizontal: 'right', vertical: 'top'}}>
+                    <MenuItem primaryText="Edit" onClick={this.handleOpen}/>
+                    <MenuItem primaryText="Remove" onClick={this.handleOpenDelete}/>
+                  </IconMenu> }
+            />
+            <CardMedia>
+              <img style={{height:230}} src="https://i.ytimg.com/vi/zdpJy70Ou48/maxresdefault.jpg" />
+            </CardMedia>
+            <CardTitle className="col-lg-9 column" titleStyle={{fontSize: 18}} title={<b><a onClick={this.handleOpenRecipie}>Spunch cake</a></b>} subtitle="Cake" />
+            <CardActions className="col-lg-3 column" style={{marginTop:15}}>
+              <IconButton onClick={this._changeLikeState} tooltip={'Like'} touch={true} tooltipPosition="bottom-right">
+                  <FavIcon onClick={this._changeLikeState} viewBox="0 0 20 30" color={deepOrange400} />
+              </IconButton>
+            </CardActions>
+            <CardText className="col-lg-12 column" style={{whiteSpace: 'nowrap',overflow: 'hidden',textOverflow: 'ellipsis'}}>
+              Ingredients (Serves: 8)
+                225g (8 oz) self-raising flour
+                225g (8 oz) butter, at room temperature
+                225g (8 oz) caster sugar
+                4 eggs
+                1 teaspoon baking powder
+
+              Method
+                Preheat the oven to 180 degrees C / gas mark 4.
+                Measure all the ingredients into a large bowl.
+                Mix all of the ingredients using a electric whisk.
+                Pour the mixture into 2 non-stick 7 inch (18cm) tins.
+                Place them in the oven till golden brown 15-25 minutes.
+                Cool on a wire rack before serving.
+            </CardText>
+          </Card>
+        </div>
+
+        <Dialog
+          title="Modify Your Recipie"
+          actions={updateActions}
+          modal={false}
+          open={this.state.editOpen}
+          onRequestClose={this.handleClose}>
+          <TextField hintText="Update your status" multiLine={false} fullWidth={true} ref="EditBox" defaultValue={this.props.postText}/>
+        </Dialog>
+
+        <Dialog
+          title="Delete Recipie"
+          actions={confirmDeleteActions}
+          modal={false}
+          open={this.state.deleteOpen}
+          onRequestClose={this.handleClose}>
+            Are you sure you want to delete this recipie?
+        </Dialog>
+
+        <Dialog
+          title="Spunch cake"
+          actions={showRecipieActions}
+          modal={false}
+          open={this.state.recipieOpen}
+          onRequestClose={this.handleClose}>
+            ssssssssssssssssssssssssssssssssssssss
+        </Dialog>
       </div>
     );
   }
