eed890d3fc5b3fc8cefd40f7a04f2bf650ded59c
TRomesh <tharakaromesh@gmail.com>
2017-06-02 16:26:48 +0530

    add sign out function

--- client/components/navigation/navigation.js
+++ client/components/navigation/navigation.js
@@ -2,6 +2,11 @@ import React,{Component} from 'react';
 import AppBar from 'material-ui/AppBar';
 import FlatButton from 'material-ui/FlatButton';
 import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import Toggle from 'material-ui/Toggle';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 const styles = {
@@ -13,7 +18,6 @@ const styles = {
 class Navigation extends Component{
 
 
-
    render(){
      return(
        <div>
@@ -21,9 +25,18 @@ class Navigation extends Component{
          title={<span style={styles.title}>RecipesHub</span>}
          iconElementLeft={<IconButton></IconButton>}
          iconElementRight={
-           <div style={{marginRight:'0px',marginTop:'8px'}}>
-             <Link to="/home"><FlatButton label="Home" /></Link>
+           <div>
+             <Link to="/home"><FlatButton label="Home"/></Link>
              <Link to="/account"><FlatButton label="Account" /></Link>
+             <IconMenu
+               iconButtonElement={
+                 <IconButton><MoreVertIcon /></IconButton>
+               }
+               targetOrigin={{horizontal: 'right', vertical: 'top'}}
+               anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+             >
+               <Link to="/login"><MenuItem primaryText="Sign out" onTouchTap={e=>{localStorage.clear();}}/></Link>
+             </IconMenu>
            </div>
        }/>
          {this.props.children}
